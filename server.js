ecf5608908e142c1e4525ce589d98b6575dd5b41
Ikshana <sherin.385558@gmail.com>
2026-06-09 11:15:42 +0530

    Initial commit - Coffee Rating Application

---
+++ server.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Coffee = require("./models/Coffee");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+
+// Home Page
+app.get("/", async (req, res) => {
+
+  let coffees = await Coffee.find();
+
+  if (coffees.length === 0) {
+
+    await Coffee.insertMany([
+      { name: "Espresso" },
+      { name: "Latte" },
+      { name: "Cappuccino" }
+    ]);
+
+    coffees = await Coffee.find();
+  }
+
+  res.render("index", { coffees });
+});
+
+
+// Vote Route
+app.post("/vote/:id", async (req, res) => {
+
+  await Coffee.findByIdAndUpdate(
+    req.params.id,
+    { $inc: { votes: 1 } }
+  );
+
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Running");
+});
